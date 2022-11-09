74e8b5b412a2ded855c2c14f2122962f2c6e43ae
Abdullah Karaman <abdullahkaramaniletisim@gmail.com>
2022-11-09 15:28:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        This website is under construction.
+      </p>
+    </div>
+  );
+}
+
+export default App;
