22ca621ac5a133e28323f02dd9b9c1d25b32704b
Abdullah Karaman <abdullahkaramaniletisim@gmail.com>
2022-11-09 15:59:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <p>
-        This website is under construction.
+        This website is under construction and will be ready soon.
       </p>
     </div>
   );
